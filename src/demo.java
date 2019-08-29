4277b05d550b731c5a6fd6d57c71565385cc7f96
hcongress <hunterjc28@gmail.com>
2019-08-29 11:50:43 -0700

    changes

--- src/demo.java
+++ src/demo.java
@@ -2,6 +2,6 @@
 public class demo {
  public static void main(String[] args){
     System.out.println("Hello World!");
-    System.out.println("This is the change.")
+    System.out.println("This is the change.=>")
  }
 }
